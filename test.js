8cba1d5a3982063935a528eede64b38293d43461
jbqmbr7pdm-maker <jbqmbr7pdm@privaterelay.appleid.com>
2026-02-04 10:16:45 +0000

    Add wget for buy-servers.js in main function

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 /** @param {NS} ns */
 export async function main(ns) {
   ns.wget("https://raw.githubusercontent.com/jbqmbr7pdm-maker/bbrnr/refs/heads/main/test2.js","test2.js");
+  ns.wget("https://raw.githubusercontent.com/jbqmbr7pdm-maker/bbrnr/refs/heads/main/buy-servers.js","buy-servers.js");
 }
